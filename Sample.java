48ef27c6f9c3686b8f5f3c0c6d064301dda40d1f
Palak1317 <palakpatel5550@gmail.com>
2025-04-06 16:07:19 +0530

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Sample {
+    public static void main(String [] args)
+    {
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Lets develope an app");
+        obj.close();
+    }
+}
